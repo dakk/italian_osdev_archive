6f0024200bbbfa0fafadc6a76bc38fe7710934f2
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-04-05 15:33:05 +0200

    remove duplicated minirighi

--- minirighi-distrib/lib/errno/errno.c
+++
@@ -1,9 +0,0 @@
-/*!	\file lib/errno.c
- *	\brief Global error value.
- *	\author Andrea Righi <righiandr@users.sf.net>
- *	\date Last update: 2004-01-25
- *	\note Copyright (&copy;) 2003 Andrea Righi
- *
- */
-
-volatile int errno = 0;
