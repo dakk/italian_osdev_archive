c32b182b01556810711b145997519d571ac83587
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-04-05 15:40:36 +0200

    remove duplicated code

--- aleos_0/include/sys.h
+++
@@ -1,5 +0,0 @@
-/* AleOS system header                  *
- * Copyright (c) 2005 Alessandro Presta */
-
-/* halt function */
-#define halt() __asm__ __volatile__ ( "hlt" )
--- bewos-0.1b/BeWos/include/string.h
+++
@@ -1,3 +0,0 @@
-int strlen(const char *str);
-int stringcmp(const char *str1, const char *str2, int n);
-
